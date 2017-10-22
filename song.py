aa74a7f775d81d1c177805573d6bab35f4284265
Alien Lien <alienlien@gmail.com>
2017-10-22 23:07:27 +0800

    Add song loader with profile and feature getting functions

--- song.py
+++ song.py
@@ -2,25 +2,45 @@
 import json
 import requests
 
-SONG_PROFILE_URL = 'https://www.indievox.com/api/mir/song/profile/'
+PROFILE_URL = 'https://www.indievox.com/api/mir/song/profile/'
+FEATURE_URL = 'https://www.indievox.com/api/mir/song/feature/'
 APP_ID = 'P300000045'
 APP_SECRET = '9bdecdb004682865260c9d2a5cc71f0d'
 SONG_ID = '110740'
 
 
-def get_profile(song_id,
-                profile_url=SONG_PROFILE_URL,
-                app_id=APP_ID,
-                app_secret=APP_SECRET):
+class SongLoader(object):
+    def __init__(self, app_id, app_secret, profile_url, feature_url):
+        self.app_id = app_id
+        self.app_secret = app_secret
+        self.profile_url = profile_url
+        self.feature_url = feature_url
+
+    def get_profile(self, song_id):
+        return get(song_id, self.app_id, self.app_secret, self.profile_url)
+
+    def get_feature(self, song_id):
+        return get(song_id, self.app_id, self.app_secret, self.feature_url)
+
+
+def get(song_id, app_id, app_secret, url):
     params = {
         'app_id': app_id,
         'app_secret': app_secret,
     }
     # TODO: Add error handling for http requests.
-    result = requests.get('{}/{}'.format(profile_url, song_id), params=params)
+    result = requests.get('{}/{}'.format(url, song_id), params=params)
     return result.json()
 
 
 if __name__ == '__main__':
-    prof = get_profile(SONG_ID)
+    loader = SongLoader(
+        app_id=APP_ID,
+        app_secret=APP_SECRET,
+        profile_url=PROFILE_URL,
+        feature_url=FEATURE_URL)
+
+    prof = loader.get_profile(SONG_ID)
     print(json.dumps(prof, indent=2, ensure_ascii=False))
+    feature = loader.get_feature(SONG_ID)
+    print(json.dumps(feature, indent=2, ensure_ascii=False))
